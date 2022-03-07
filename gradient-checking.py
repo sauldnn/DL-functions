35382814c144ca474a32ad235a8e2a890d188d6d
sauldnn <saul.dnn7@gmail.com>
2022-03-07 14:48:07 -0600

    generalization of gradient checking for n dimensions

--- gradient-checking.py
+++ gradient-checking.py
@@ -63,3 +63,53 @@ def gradient_check(x, theta, epsilon=1e-7, print_msg=False):
             print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
     
     return difference
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7, print_msg=False):
+    """
+    Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+    grad -- output of backward_propagation_n, contains gradients of the cost with respect to the parameters. 
+    x -- input datapoint, of shape (input size, 1)
+    y -- true "label"
+    epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+    
+    Returns:
+    difference -- difference (2) between the approximated gradient and the backward propagation gradient
+    """
+    
+    # Set-up variables
+    parameters_values, _ = dictionary_to_vector(parameters)
+    
+    grad = gradients_to_vector(gradients)
+    num_parameters = parameters_values.shape[0]
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+    
+    # Compute gradapprox
+    for i in range(num_parameters):
+        
+        theta_plus = np.copy(parameters_values)
+        theta_plus[i] = theta_plus[i] + epsilon
+        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(theta_plus))
+        
+        
+        theta_minus = np.copy(parameters_values)
+        theta_minus[i] = theta_minus[i] - epsilon
+        J_minus[i] ,_ = forward_propagation_n(X, Y, vector_to_dictionary(theta_minus))
+        
+        gradapprox[i] = (J_plus[i] - J_minus[i])/(2*epsilon)
+        
+    numerator = np.linalg.norm(grad - gradapprox)
+    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)
+    difference = numerator/denominator
+    
+    if print_msg:
+        if difference > 2e-7:
+            print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+        else:
+            print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+
+    return difference
