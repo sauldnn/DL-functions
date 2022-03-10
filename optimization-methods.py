9c260a917c1b9f6741c098d2972d4109b4783599
sauldnn <saul.dnn7@gmail.com>
2022-03-09 20:51:22 -0600

    Initialization for Adam optimizer

--- optimization-methods.py
+++ optimization-methods.py
@@ -130,3 +130,93 @@ def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
         
     return parameters, v
 
+def initialize_adam(parameters) :
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL" 
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+    
+    Returns: 
+    v -- python dictionary that will contain the exponentially weighted average of the gradient. Initialized with zeros.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient. Initialized with zeros.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+    
+    L = len(parameters) // 2 # number of layers in the neural networks
+    v = {}
+    s = {}
+    
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(1, L + 1):
+
+        v["dW" + str(l)] = np.zeros((parameters['W'+str(l)].shape[0], parameters['W'+str(l)].shape[1]))
+        v["db" + str(l)] = np.zeros((parameters['b'+str(l)].shape[0], parameters['b'+str(l)].shape[1]))
+        s["dW" + str(l)] = np.zeros((parameters['W'+str(l)].shape[0], parameters['W'+str(l)].shape[1]))
+        s["db" + str(l)] = np.zeros((parameters['b'+str(l)].shape[0], parameters['b'+str(l)].shape[1]))
+    
+    return v, s
+
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01,
+                                beta1 = 0.9, beta2 = 0.999,  epsilon = 1e-8):
+    """
+    Update parameters using Adam
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    t -- Adam variable, counts the number of taken steps
+    learning_rate -- the learning rate, scalar.
+    beta1 -- Exponential decay hyperparameter for the first moment estimates 
+    beta2 -- Exponential decay hyperparameter for the second moment estimates 
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    """
+    
+    L = len(parameters) // 2                 # number of layers in the neural networks
+    v_corrected = {}                         # Initializing first moment estimate, python dictionary
+    s_corrected = {}                         # Initializing second moment estimate, python dictionary
+    
+    # Perform Adam update on all parameters
+    for l in range(1, L + 1):
+        # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
+        
+        v['dW' + str(l)] = beta1 * v['dW' + str(l)] + (1-beta1) * grads['dW' + str(l)]
+        v['db' + str(l)] = beta1 * v['db' + str(l)] + (1-beta1) * grads['db' + str(l)]
+        
+        
+        v_corrected['dW' + str(l)] = v['dW' + str(l)] / (1-beta1**t)
+        v_corrected['db' + str(l)] = v['db' + str(l)] / (1-beta1**t)
+        
+        
+        s['dW' + str(l)] = beta2 * s['dW' + str(l)] + (1-beta2) * (np.square(grads['dW' + str(l)]))
+        s['db' + str(l)] = beta2 * s['db' + str(l)] + (1-beta2) * (np.square(grads['db' + str(l)]))
+        
+        
+        s_corrected['dW' + str(l)] = s['dW' + str(l)]/(1-beta2**t)
+        s_corrected['db' + str(l)] = s['db' + str(l)]/(1-beta2**t)
+        
+        
+        parameters['W' + str(l)] = parameters['W' + str(l)] - learning_rate * v_corrected['dW' + str(l)]/(np.sqrt(s_corrected['dW' + str(l)]) + epsilon)
+        parameters['b' + str(l)] = parameters['b' + str(l)] - learning_rate * v_corrected['db' + str(l)]/(np.sqrt(s_corrected['db' + str(l)]) + epsilon)
+
+    return parameters, v, s, v_corrected, s_corrected
+
