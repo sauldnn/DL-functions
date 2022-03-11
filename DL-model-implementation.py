0f8086a700c5dc3cda5811fef53a8d844661d27d
sauldnn <saul.dnn7@gmail.com>
2022-03-10 23:59:17 -0600

    L-layer model

--- DL-model-implementation.py
+++ DL-model-implementation.py
@@ -87,3 +87,59 @@ def plot_costs(costs, learning_rate=0.0075):
     plt.xlabel('iterations (per hundreds)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
+
+# GRADED FUNCTION: L_layer_model
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    np.random.seed(1)
+    costs = []                         # keep track of cost
+    
+    # Parameters initialization.
+
+    parameters = initialize_parameters_deep(layers_dims)
+    
+    
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        
+        AL, caches = L_model_forward(X, parameters)
+        
+        # Compute cost.
+
+        cost = compute_cost(AL, Y)
+        
+    
+        # Backward propagation.
+
+        grads = L_model_backward(AL, Y, caches)
+        
+ 
+        # Update parameters.
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+                
+        # Print the cost every 100 iterations
+        if print_cost and i % 100 == 0 or i == num_iterations - 1:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0 or i == num_iterations:
+            costs.append(cost)
+    
+    return parameters, costs
