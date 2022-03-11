7a46ee911c3f31cd592cffc40f56dac165996bb2
sauldnn <saul.dnn7@gmail.com>
2022-03-10 23:45:15 -0600

    cost function for classification

--- step-by-step.py
+++ step-by-step.py
@@ -85,3 +85,28 @@ def L_model_forward(X, parameters):
     
           
     return AL, caches
+
+# GRADED FUNCTION: compute_cost for classification
+
+def compute_cost(AL, Y):
+    """
+    Implement the cost function defined by equation (7).
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+    
+    m = Y.shape[1]
+
+    
+    cost = - (1/m) * np.sum((np.dot(Y, np.log(AL).T)) + (np.dot(1-Y, np.log(1-AL).T))) 
+    
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+
+    
+    return cost
